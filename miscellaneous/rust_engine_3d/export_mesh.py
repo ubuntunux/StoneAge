9615c81098663b49e4b93be7d4a366e638e198c9
ubuntunux <ubuntunux@gmail.com>
2024-01-09 02:21:44 +0900

    update script

--- miscellaneous/rust_engine_3d/export_mesh.py
+++ miscellaneous/rust_engine_3d/export_mesh.py
@@ -1,48 +1,86 @@
+from enum import Enum
+import os
 import json
 
 import bpy
 import bpy_extras
 
 
+class ResouerceTypeInfo:
+    def __init__(self, resource_dirname, resource_ext, external_ext):
+        self.resource_dirname = resource_dirname
+        self.resource_ext = resource_ext
+        self.external_ext = external_ext
+        
+
+class ResourceType(Enum):
+    MODEL = ResouerceTypeInfo('models', '.model', '.gltf')
+    MESH = ResouerceTypeInfo('meshes', '.mesh', '.gltf')
+    
+    
+class ResourceInfo:
+    def __init__(self, resource_type, filepath):
+        # initialize
+        self.resource_type = resource_type
+        self.filepath = filepath
+        self.external_filepath = ''
+        self.resource_name = ''
+        self.resource_filepath = ''
+        
+        resource_type_info = resource_type.value
+        
+        # extract filename
+        dirname, filename = os.path.split(filepath)
+        basename = os.path.splitext(filename)[0]
+        external_filename = filename
+        resource_filename = filename
+        external_file_basename = basename + resource_type_info.external_ext
+        resource_file_basename = basename + resource_type_info.resource_ext
+            
+        # external filepath
+        self.external_filepath = os.path.join(dirname, external_file_basename)
+        
+        # extract resource name, filepath
+        while True:
+            head, tail = os.path.split(dirname)
+            if '' == tail or resource_type_info.resource_dirname == tail:
+                resource_dirname = resource_filepath = os.path.split(head)[0]
+                relative_dirname = os.path.split(os.path.relpath(filepath, head))[0]                
+                self.resource_name = os.path.join(relative_dirname, basename)
+                self.resource_filepath = os.path.join(resource_dirname, os.path.join(relative_dirname, resource_file_basename))
+                break
+            dirname = head
+
+
 def save(operator, context, filepath='', **keywords):
     if filepath == '':
         return {'FINISHED'}
-
+    
+    resource_info = ResourceInfo(ResourceType.MESH, filepath)
+    print(resource_info.__dict__)
+    
     context.window.cursor_set('WAIT')
 
     scene = context.scene
     objects = scene.objects
     mesh_objects = [ob for ob in objects if ob.type == 'MESH']
-    empty_objects = [ob for ob in objects if ob.type == 'EMPTY']
-    light_objects = [ob for ob in objects if ob.type == 'LIGHT']
-    camera_objects = [ob for ob in objects if ob.type == 'CAMERA']
-
-    context.window.cursor_set('DEFAULT')
 
     game_scene_data = {
-        "_scene_data_name": "intro_stage",
-        "_player": {
-            "player": {"_character_data_name": "jack", "_position": [0.0, 1.0, 0.0], "_rotation": [0.0, 0.0, 0.0],
-                       "_scale": [0.25, 0.25, 0.25]}
-        },
-        "_characters": {
-            "enemy00": {"_character_data_name": "pickle", "_position": [2.0, 1.0, 0.0], "_rotation": [0.0, 0.0, 0.0],
-                        "_scale": [1.0, 1.0, 1.0]},
-            "enemy01": {"_character_data_name": "pickle", "_position": [-2.0, 1.0, 0.0], "_rotation": [0.0, 0.0, 0.0],
-                        "_scale": [1.0, 1.0, 1.0]},
-            "enemy02": {"_character_data_name": "pickle", "_position": [0.0, 1.0, 0.0], "_rotation": [0.0, 0.0, 0.0],
-                        "_scale": [1.0, 1.0, 1.0]}
-        },
-        "_blocks": {
-            "rock00": {"_block_data_name": "cliff_grass", "_position": [2.0, 10.0, 0.0], "_rotation": [0.0, 0.0, 0.0],
-                       "_scale": [1.0, 1.0, 1.0]},
-            "rock01": {"_block_data_name": "cliff_grass", "_position": [4.0, 12.0, 0.0], "_rotation": [0.0, 0.0, 0.0],
-                       "_scale": [1.0, 1.0, 1.0]}
-        },
-        "_start_point": [0.0, 1.0, 0.0]
+        "material_name": "common/render_static_object",
+        "material_parameters": {
+            "textureBase": "environments/desert_ground",
+            "textureMaterial": "common/default_m",
+            "textureNormal": "common/default_n"
+        }
     }
 
-    with open(filepath, 'w') as f:
-        f.write(json.dumps(game_scene_data, sort_keys=True, indent=4))
 
+#    with open(resource_info.resource_filepath, 'w') as f:
+#        f.write(json.dumps(game_scene_data, sort_keys=True, indent=4))
+
+    context.window.cursor_set('DEFAULT')
     return {'FINISHED'}
+
+
+
+save(None, bpy.context, filepath=bpy.data.filepath)
