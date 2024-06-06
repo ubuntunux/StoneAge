f43d64ed1f90ae57eb12e78b7a0fa242193b1b3a
ubuntunux <ubuntunux@gmail.com>
2024-06-06 13:28:33 +0900

    Revert "update"
    This reverts commit 212153dc6aa08865167902562a3d0660cafd717b.

--- src/game_module/actors/character_manager.rs
+++ src/game_module/actors/character_manager.rs
@@ -99,18 +99,12 @@ impl<'a> CharacterManager<'a> {
             character_name,
             &render_object_create_info
         );
-
-        let attack_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._attack_animation_mesh);
         let dead_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._dead_animation_mesh);
-        let hit_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._hit_animation_mesh);
         let idle_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._idle_animation_mesh);
-        let jump_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._jump_animation_mesh);
-        let power_attack_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._power_attack_animation_mesh);
-        let roll_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._roll_animation_mesh);
-        let run_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._run_animation_mesh);
-        let running_jump_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._running_jump_animation_mesh);
+        let hit_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._hit_animation_mesh);
         let walk_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._walk_animation_mesh);
-
+        let jump_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._jump_animation_mesh);
+        let attack_animation = game_resources.get_engine_resources().get_mesh_data(&character_data.borrow()._attack_animation_mesh);
         let id = self.generate_id();
         let character = newRcRefCell(Character::create_character_instance(
             self,
@@ -119,16 +113,12 @@ impl<'a> CharacterManager<'a> {
             character_name,
             character_data,
             &render_object_data,
-            attack_animation,
             dead_animation,
-            hit_animation,
             idle_animation,
-            jump_animation,
-            power_attack_animation,
-            roll_animation,
-            run_animation,
-            running_jump_animation,
+            hit_animation,
             walk_animation,
+            jump_animation,
+            attack_animation,
             self._animation_blend_masks.as_ref(),
             &character_create_info._position,
             &character_create_info._rotation,
