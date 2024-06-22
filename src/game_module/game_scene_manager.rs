e755694abcb87f0b7b3096e1c924cdf36dd7cb1f
ubuntunux <ubuntunux@gmail.com>
2024-06-22 22:53:25 +0900

    update music

--- src/game_module/game_scene_manager.rs
+++ src/game_module/game_scene_manager.rs
@@ -88,7 +88,7 @@ impl<'a> GameSceneManager<'a> {
     }
 
     pub fn play_music(&mut self, audio_name: &str) {
-        self._game_music = ptr_as_mut(self._audio_manager).create_audio_instance_from_bank(audio_name, AudioLoop::LOOP, Some(255));
+        self._game_music = ptr_as_mut(self._audio_manager).create_audio_instance_from_bank(audio_name, AudioLoop::LOOP, None);
     }
 
     pub fn get_blocks(&self) -> &HashMap<u64, RcRefCell<Block<'a>>> {
