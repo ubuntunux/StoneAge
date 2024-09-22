88f78090ca1d25a81aca6e7dc06bbb58c40f6184
ubuntunux <ubuntunux@gmail.com>
2024-09-23 02:19:39 +0900

    apply taa to ssao

--- src/render_pass/render_forward_for_light_probe.rs
+++ src/render_pass/render_forward_for_light_probe.rs
@@ -24,7 +24,7 @@ pub fn get_render_pass_data_create_info(
         .get_mut(&*render_pass_name)
         .unwrap();
     let mut pipeline_data_create_info =
-        render_pass_data_create_info.get_pipeline_data_create_info_clone("render_object");
+        render_pass_data_create_info.get_pipeline_data_create_info("render_object").clone();
     pipeline_data_create_info._pipeline_data_create_info_name = String::from("render_grass");
     pipeline_data_create_info._pipeline_vertex_shader_file = PathBuf::from("render_grass.vert");
     pipeline_data_create_info._pipeline_fragment_shader_file = PathBuf::from("render_grass.frag");
