1dc7f4e9c23bb6ca75d394050858a399bb8aec28
ubuntunux <ubuntunux@gmail.com>
2025-08-14 02:00:34 +0900

    update vegetation shader

--- resources/shaders/PolygonNatureBiomes/VegetationShader.glsl
+++ resources/shaders/PolygonNatureBiomes/VegetationShader.glsl
@@ -1,10 +1,20 @@
 #include "../../engine_resources/shaders/common/random.glsl"
 
-layout(binding = USER_BINDING_INDEX0) uniform sampler2D _LeafTex;
-layout(binding = USER_BINDING_INDEX1) uniform sampler2D textureMaterial;
+layout(binding = USER_BINDING_INDEX0) uniform sampler2D _EmissiveMask;
+layout(binding = USER_BINDING_INDEX1) uniform sampler2D _GustNoiseMap;
 layout(binding = USER_BINDING_INDEX2) uniform sampler2D _LeafNormalMap;
+layout(binding = USER_BINDING_INDEX3) uniform sampler2D _LeafTex;
+layout(binding = USER_BINDING_INDEX4) uniform sampler2D _TrunkNormalMap;
+layout(binding = USER_BINDING_INDEX5) uniform sampler2D _TunkTex;
 
 BEGIN_PUSH_CONSTANT(PushConstant_VegetationShader)
+    vec4 _BaseColour;
+    vec4 _EmissiveColour;
+    vec4 _TrunkBaseColour;
+    float _GustFreq;
+    float _GustLargeFreq;
+    float _LeafSmoothness;
+    float _TrunkSmoothness;
 END_PUSH_CONSTANT()
 
 #if SHADER_STAGE_FLAG == VERTEX
@@ -16,9 +26,14 @@ VERTEX_SHADER_MAIN()
 #elif SHADER_STAGE_FLAG == FRAGMENT
 FRAGMENT_SHADER_MAIN()
 {
-    out_base_color = texture(_LeafTex, in_vertex_output._texCoord);
+    const float blend_factor = 0.5 < in_vertex_output._color.z ? 1.0 : 0.0;
+    const vec3 base_colour = mix(GET_PUSH_CONSTANT()._TrunkBaseColour, GET_PUSH_CONSTANT()._BaseColour, blend_factor).xyz;
+    out_base_color = mix(texture(_TunkTex, in_vertex_output._texCoord), texture(_LeafTex, in_vertex_output._texCoord), blend_factor);
+    //out_base_color.xyz *= base_colour;
     out_base_color.xyz = pow(out_base_color.xyz, vec3(2.2));
-    out_material = texture(textureMaterial, in_vertex_output._texCoord);
-    out_tangent_normal = texture(_LeafNormalMap, in_vertex_output._texCoord).xyz * 2.0 - 1.0;
+    out_material = vec4(0.0);
+    // roughness
+    out_material.x = 1.0 - mix(GET_PUSH_CONSTANT()._TrunkSmoothness, GET_PUSH_CONSTANT()._LeafSmoothness, blend_factor);
+    out_tangent_normal = mix(texture(_TrunkNormalMap, in_vertex_output._texCoord).xyz * 2.0 - 1.0, texture(_LeafNormalMap, in_vertex_output._texCoord).xyz * 2.0 - 1.0, blend_factor);
 }
 #endif
--- src/render_pass/PolygonNatureBiomes/VegetationShader.rs
+++ src/render_pass/PolygonNatureBiomes/VegetationShader.rs
@@ -1,7 +1,8 @@
 use ash::vk;
+use nalgebra::Vector4;
 use serde::{Deserialize, Serialize};
 use rust_engine_3d::render_pass::render_object::common;
-use rust_engine_3d::render_pass::render_object::common::{USER_BINDING_INDEX0, USER_BINDING_INDEX1, USER_BINDING_INDEX2};
+use rust_engine_3d::render_pass::render_object::common::{USER_BINDING_INDEX0, USER_BINDING_INDEX1, USER_BINDING_INDEX2, USER_BINDING_INDEX3, USER_BINDING_INDEX4, USER_BINDING_INDEX5};
 use rust_engine_3d::renderer::push_constants::{PushConstant, PushConstantName, PushConstantParameter, PushConstant_RenderObjectBase};
 use rust_engine_3d::renderer::renderer_data::RendererData;
 use rust_engine_3d::vulkan_context::descriptor::{DescriptorDataCreateInfo, DescriptorResourceType};
@@ -12,13 +13,27 @@ use rust_engine_3d::vulkan_context::render_pass::RenderPassDataCreateInfo;
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[serde(default)]
 pub struct PushConstant_VegetationShader {
-    pub _push_constant_base: PushConstant_RenderObjectBase
+    pub _push_constant_base: PushConstant_RenderObjectBase,
+    pub _BaseColour: Vector4<f32>,
+    pub _EmissiveColour: Vector4<f32>,
+    pub _TrunkBaseColour: Vector4<f32>,
+    pub _GustFreq: f32,
+    pub _GustLargeFreq: f32,
+    pub _LeafSmoothness: f32,
+    pub _TrunkSmoothness: f32
 }
 
 impl Default for PushConstant_VegetationShader {
     fn default() -> PushConstant_VegetationShader {
         PushConstant_VegetationShader {
-            _push_constant_base: PushConstant_RenderObjectBase::default()
+            _push_constant_base: PushConstant_RenderObjectBase::default(),
+            _BaseColour: Vector4::new(1.0, 1.0, 1.0, 1.0),
+            _EmissiveColour: Vector4::new(0.0, 0.0, 0.0, 1.0),
+            _TrunkBaseColour: Vector4::new(1.0, 1.0, 1.0, 1.0),
+            _GustFreq: 0.0,
+            _GustLargeFreq: 0.0,
+            _LeafSmoothness: 0.0,
+            _TrunkSmoothness: 0.0
         }
     }
 }
@@ -31,7 +46,38 @@ impl PushConstantName for PushConstant_VegetationShader {
 
 impl PushConstant for PushConstant_VegetationShader {
     fn set_push_constant_parameter(&mut self, key: &str, value: &PushConstantParameter) -> bool {
-        self._push_constant_base.set_push_constant_parameter(key, value)
+        if "_BaseColour" == key {
+            if let PushConstantParameter::Float4(value) = value {
+                self._BaseColour = *value;
+            }
+        } else if "_EmissiveColour" == key {
+            if let PushConstantParameter::Float4(value) = value {
+                self._EmissiveColour = *value;
+            }
+        } else if "_TrunkBaseColour" == key {
+            if let PushConstantParameter::Float4(value) = value {
+                self._TrunkBaseColour = *value;
+            }
+        } else if "_GustFreq" == key {
+            if let PushConstantParameter::Float(value) = value {
+                self._GustFreq = *value;
+            }
+        } else if "_GustLargeFreq" == key {
+            if let PushConstantParameter::Float(value) = value {
+                self._GustLargeFreq = *value;
+            }
+        } else if "_LeafSmoothness" == key {
+            if let PushConstantParameter::Float(value) = value {
+                self._LeafSmoothness = *value;
+            }
+        } else if "_TrunkSmoothness" == key {
+            if let PushConstantParameter::Float(value) = value {
+                self._TrunkSmoothness = *value;
+            }
+        } else {
+            return self._push_constant_base.set_push_constant_parameter(key, value);
+        }
+        true
     }
 }
 
@@ -43,14 +89,14 @@ pub fn get_descriptor_data_create_infos() -> Vec<DescriptorDataCreateInfo> {
     vec![
         DescriptorDataCreateInfo {
             _descriptor_binding_index: USER_BINDING_INDEX0,
-            _descriptor_name: String::from("_LeafTex"),
+            _descriptor_name: String::from("_EmissiveMask"),
             _descriptor_resource_type: DescriptorResourceType::Texture,
             _descriptor_shader_stage: vk::ShaderStageFlags::FRAGMENT,
             ..Default::default()
         },
         DescriptorDataCreateInfo {
             _descriptor_binding_index: USER_BINDING_INDEX1,
-            _descriptor_name: String::from("textureMaterial"),
+            _descriptor_name: String::from("_GustNoiseMap"),
             _descriptor_resource_type: DescriptorResourceType::Texture,
             _descriptor_shader_stage: vk::ShaderStageFlags::FRAGMENT,
             ..Default::default()
@@ -58,6 +104,27 @@ pub fn get_descriptor_data_create_infos() -> Vec<DescriptorDataCreateInfo> {
         DescriptorDataCreateInfo {
             _descriptor_binding_index: USER_BINDING_INDEX2,
             _descriptor_name: String::from("_LeafNormalMap"),
+            _descriptor_resource_type: DescriptorResourceType::Texture,
+            _descriptor_shader_stage: vk::ShaderStageFlags::FRAGMENT,
+            ..Default::default()
+        },
+        DescriptorDataCreateInfo {
+            _descriptor_binding_index: USER_BINDING_INDEX3,
+            _descriptor_name: String::from("_LeafTex"),
+            _descriptor_resource_type: DescriptorResourceType::Texture,
+            _descriptor_shader_stage: vk::ShaderStageFlags::FRAGMENT,
+            ..Default::default()
+        },
+        DescriptorDataCreateInfo {
+            _descriptor_binding_index: USER_BINDING_INDEX4,
+            _descriptor_name: String::from("_TrunkNormalMap"),
+            _descriptor_resource_type: DescriptorResourceType::Texture,
+            _descriptor_shader_stage: vk::ShaderStageFlags::FRAGMENT,
+            ..Default::default()
+        },
+        DescriptorDataCreateInfo {
+            _descriptor_binding_index: USER_BINDING_INDEX5,
+            _descriptor_name: String::from("_TunkTex"),
             _descriptor_resource_type: DescriptorResourceType::Texture,
             _descriptor_shader_stage: vk::ShaderStageFlags::FRAGMENT,
             ..Default::default()
