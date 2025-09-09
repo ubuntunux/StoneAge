456def645dd4da262871893bc2c143aa560cec65
ubuntunux <ubuntunux@gmail.com>
2025-09-10 02:59:17 +0900

    update

--- src/game_module/game_constants.rs
+++ src/game_module/game_constants.rs
@@ -33,7 +33,7 @@ pub const SLOPE_SPEED: f32 = 1.0;
 pub const SLOPE_ANGLE: f32 = 0.707;
 pub const SLOPE_VELOCITY_DECAY: f32 = 100.0;
 pub const HIT_VELOCITY_SPEED: f32 = 20.0;
-pub const HIT_VELOCITY_DECAY: f32 = 100.0;
+pub const HIT_VELOCITY_DECAY: f32 = 200.0;
 pub const ATTACK_DELAY: f32 = 0.0;
 
 // stamina
