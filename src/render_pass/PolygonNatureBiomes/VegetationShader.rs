3f4f7648fcc9f2683d3373bfbb1a3af2c4d8dfcd
ubuntunux <ubuntunux@gmail.com>
2025-09-13 01:24:45 +0900

    Revert "update low polygon"
    This reverts commit 03d3548e657ccdb888adef065fe862160557aeee.

--- src/render_pass/PolygonNatureBiomes/VegetationShader.rs
+++ src/render_pass/PolygonNatureBiomes/VegetationShader.rs
@@ -207,7 +207,7 @@ pub fn get_descriptor_data_create_infos() -> Vec<DescriptorDataCreateInfo> {
 pub fn get_render_pass_data_create_infos(renderer_data: &RendererData) -> Vec<RenderPassDataCreateInfo> {
     common::get_render_pass_data_create_infos(
         renderer_data,
-        vk::CullModeFlags::NONE,
+        vk::CullModeFlags::BACK,
         "PolygonNatureBiomes_VegetationShader",
         "PolygonNatureBiomes/VegetationShader.vert",
         "PolygonNatureBiomes/VegetationShader.frag",
