5eb9f37f491136c0733b51bb2cac8d214837bfdc
ubuntunux <ubuntunux@gmail.com>
2025-12-17 03:46:30 +0900

    update toolbox

--- src/game_module/widgets/toolbox_widget.rs
+++ src/game_module/widgets/toolbox_widget.rs
@@ -9,6 +9,58 @@ use rust_engine_3d::utilities::system::{ptr_as_mut, RcRefCell};
 use rust_engine_3d::vulkan_context::vulkan_context::get_color32;
 use crate::game_module::actors::character::Character;
 
+const ITEM_HEIGHT: f32 = 100.0;
+
+pub struct ToolboxItem<'a> {
+    pub _layout: Rc<WidgetDefault<'a>>,
+}
+
+impl<'a> ToolboxItem<'a> {
+    pub fn create_toolbox_item(
+        parent_widget: &mut WidgetDefault<'a>
+    ) -> ToolboxItem<'a> {
+        let layout = UIManager::create_widget("toolbox_item", UIWidgetTypes::Default);
+        let layout_mut = ptr_as_mut(layout.as_ref());
+        let ui_component = layout_mut.get_ui_component_mut();
+        ui_component.set_layout_type(UILayoutType::BoxLayout);
+        ui_component.set_layout_orientation(Orientation::HORIZONTAL);
+        ui_component.set_halign(HorizontalAlign::LEFT);
+        ui_component.set_valign(VerticalAlign::TOP);
+        ui_component.set_size_hint_x(Some(1.0));
+        ui_component.set_size_y(ITEM_HEIGHT);
+        ui_component.set_renderable(false);
+        parent_widget.add_widget(&layout);
+
+        let item_icon = UIManager::create_widget("icon", UIWidgetTypes::Default);
+        let ui_component = ptr_as_mut(item_icon.as_ref()).get_ui_component_mut();
+        ui_component.set_layout_type(UILayoutType::BoxLayout);
+        ui_component.set_layout_orientation(Orientation::HORIZONTAL);
+        ui_component.set_size_x(ITEM_HEIGHT);
+        ui_component.set_size_y(ITEM_HEIGHT);
+        ui_component.set_color(get_color32(50, 50, 50, 128));
+        ui_component.set_border_color(get_color32(0, 0, 0, 255));
+        ui_component.set_margin(10.0);
+        ui_component.set_round(5.0);
+        layout_mut.add_widget(&item_icon);
+
+        let ingredient_layout = UIManager::create_widget("ingredient", UIWidgetTypes::Default);
+        let ui_component = ptr_as_mut(ingredient_layout.as_ref()).get_ui_component_mut();
+        ui_component.set_layout_type(UILayoutType::BoxLayout);
+        ui_component.set_layout_orientation(Orientation::HORIZONTAL);
+        ui_component.set_size_x(ITEM_HEIGHT * 2.0);
+        ui_component.set_size_y(ITEM_HEIGHT);
+        ui_component.set_color(get_color32(50, 50, 50, 128));
+        ui_component.set_border_color(get_color32(0, 0, 0, 255));
+        ui_component.set_margin(10.0);
+        ui_component.set_round(5.0);
+        layout_mut.add_widget(&ingredient_layout);
+
+        ToolboxItem {
+            _layout: layout,
+        }
+    }
+}
+
 pub struct ToolboxWidget<'a> {
     pub _parent_widget: *const WidgetDefault<'a>,
     pub _layer: Rc<WidgetDefault<'a>>,
@@ -31,6 +83,11 @@ impl<'a> ToolboxWidget<'a> {
         ui_component.set_color(get_color32(50, 50, 50, 128));
         ui_component.set_border_color(get_color32(0, 0, 0, 255));
         ui_component.set_round(5.0);
+
+        {
+            let layer_mut = ptr_as_mut(layer.as_ref());
+            ToolboxItem::create_toolbox_item(layer_mut);
+        }
 
         ToolboxWidget {
             _parent_widget: parent_widget,
