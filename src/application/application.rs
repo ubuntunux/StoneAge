0c6037b867a6f044f369abdfa30fa65bad2e47f7
ubuntunux <ubuntunux@gmail.com>
2026-09-23 02:01:32 +0900

    fix bug down frame rate by point light count

--- src/application/application.rs
+++ src/application/application.rs
@@ -242,7 +242,7 @@ pub fn run_application() {
         #[cfg(not(target_os = "android"))]
         {
             constants::VULKAN_API_VERSION = vk::make_api_version(0, 1, 3, 0);
-            constants::ENABLE_IMMEDIATE_MODE = false;
+            constants::ENABLE_IMMEDIATE_MODE = true;
             //constants::TARGET_FRAME_RATE = 25; // Set e.g. 15, 20, 30 to test low frame rate (0 = unlimited / VSync default)
             constants::IS_CONCURRENT_MODE = true;
         }
@@ -257,6 +257,8 @@ pub fn run_application() {
         } else {
             constants::DEBUG_MESSAGE_LEVEL = vk::DebugUtilsMessageSeverityFlagsEXT::ERROR;
         }
+
+
 
         constants::REQUIRED_DEVICE_EXTENSIONS = vec![
             "VK_KHR_swapchain".to_string(),
